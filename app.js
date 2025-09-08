715f41fc04d1d2c8439305baaa69013032314f9d
Pedro Ramos de Sa <pedro.ramosdesa2006@gmail.com>
2025-09-08 16:45:04 +0200

    #2 modfication fichier app.js et creation index.html

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('TP CIEL');
+
+/*  *********************** Serveur Web ***************************   */
+
+var express = require('express'); 
+var port = 80;
+
+var exp = express(); 
+
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client'); 
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+}); 
